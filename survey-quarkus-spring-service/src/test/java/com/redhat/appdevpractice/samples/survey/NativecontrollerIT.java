4913366a516490a2d3fb1c04812860371c1e229b
Rashan Smith <rassmith@new-host.home>
2020-04-29 16:57:23 -0400

    wip temp

---
+++ survey-quarkus-spring-service/src/test/java/com/redhat/appdevpractice/samples/survey/NativecontrollerIT.java
@@ -0,0 +1,9 @@
+package com.redhat.appdevpractice.samples.survey;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativecontrollerIT extends controllerTest {
+
+    // Execute the same tests but in native mode.
+}
